247634515ff0996749c5b97a98346380794a1788
Omenkish <omenkish@gmail.com>
2020-04-03 22:26:05 +0100

    implemented header image and header background image

--- screens/Review.jsx
+++ screens/Review.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-import { globalStyles } from '../styles/global';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/Card';
 
 export default function Review({route: { params }}) {
   const { title, rating, body } = params;
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>{title}</Text>
-      <Text style={globalStyles.titleText}>{body}</Text>
-      <Text style={globalStyles.titleText}>{rating}</Text>
+      <Card>
+        <Text style={globalStyles.titleText}>{title}</Text>
+        <Text style={globalStyles.titleText}>{body}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]}/>
+        </View>
+      </Card>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee'
+
+  }
+});
--- shared/Header.jsx
+++ shared/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground , Image, StyleSheet, Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ navigation, title }) {
@@ -9,12 +9,13 @@ export default function Header({ navigation, title }) {
   };
 
   return (
-    <View style={styles.header}>
+    <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
       <MaterialIcons name='menu' size={28} style={styles.icon} onPress={toggleDrawer} />
-      <View>
+      <View style={styles.headerTitle}>
+        <Image source={require('../assets/heart_logo.png')} style={styles.headerImage}/>
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -35,5 +36,13 @@ const styles = StyleSheet.create({
   icon: {
     position: 'absolute',
     left: 0,
+  },
+  headerTitle: {
+    flexDirection: 'row'
+  },
+  headerImage: {
+    width:26,
+    height: 26,
+    marginHorizontal: 10
   }
 });
